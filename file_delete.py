6e7cd7b9ccb54caab5dabc355e4b5eefbe81bf3c
tyler-shen <tshen@ualberta.ca>
2024-03-23 14:35:26 -0600

    Update file_delete.py

--- file_delete.py
+++ file_delete.py
@@ -1,6 +1,10 @@
 # import os
 # os.remove("test.txt")
 
+# file = open("test.txt", "w")
+# file.write("new line")
+# file.close()
+
 import os
 file_name = "test.txt"
 if os.path.exists(file_name):
@@ -8,3 +12,11 @@ if os.path.exists(file_name):
 else:
   # raise Exception("file doesn't exist!")
   print(f"file {file_name} doesn't exist!")
+
+# file_name = "test.txt"
+# try:
+#     os.remove(file_name)
+# except FileNotFoundError:
+#     print("file not existed")
+# except:
+#     print("something goes wrong")
