9540f559b0aafc24702ee830cdc6bca4a3c8577a
tyler-shen <tshen@ualberta.ca>
2024-10-18 20:20:12 -0600

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -2,6 +2,5 @@ print("mutliplication table:")
 
 for row in range(1, 10):
     for col in range(1, row+1):
-        print(f"{row} \u2715 {col}={row*col:3d}", end='  ')
-    print()
+        print(f"{col}x{row}={row*col:2d}", end='  ')
     print()
